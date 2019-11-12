18628da99b7191f6232373fd51fad5f91551f7c3
RansonDrew <46309170+RansonDrew@users.noreply.github.com>
2019-11-11 19:18:03 -0500

    Update wpgetpost.py
    Functionalized everything and added a function to format text to a fixed width.

--- wpgetpost.py
+++ wpgetpost.py
@@ -1,18 +1,35 @@
 import requests
 import datetime
+import textwrap
 
 def swaphtml(intext):
      outtext = intext.replace('&#8217;',"'")
      outtext = outtext.replace('&#8230;',"...")
      outtext = outtext.replace('&#8220;','"')
      outtext = outtext.replace('&#8221;','"')
+     outtext = outtext.replace('<p>','')
+     outtext = outtext.replace('</p>','')
      return outtext
 
-response = requests.get('https://drewfowler.com/wp-json/wp/v2/posts/?categories=8')
-posts = response.json()
+def fmttextpost(intext,txtwidth):
+     orgparatext = intext.splitlines(keepends=True)
+     newtxt = []
+     for graf in orgparatext:
+          graf = swaphtml(graf)
+          graf = textwrap.fill(graf,txtwidth)
+          newtxt.append(graf)
+     a = '\n'
+     finaltxt = a.join(newtxt)
+     return finaltxt
 
-for post in posts:
-	postdt = datetime.datetime.strptime(post.get('date'), '%Y-%m-%dT%H:%M:%S')
-	print('Post title:',swaphtml(post.get('title').get('rendered')))
-	print('Post date:',postdt)
-	print('Post content:', swaphtml(post.get('content').get('rendered')))
+def getwpposts():
+     response = requests.get('https://drewfowler.com/wp-json/wp/v2/posts/?categories=8')
+     posts = response.json()
+     return posts
+
+def printwpposts(wpposts):
+     for wppost in wpposts:
+	     postdt = datetime.datetime.strptime(wppost.get('date'), '%Y-%m-%dT%H:%M:%S')
+	     print('Post title:',swaphtml(wppost.get('title').get('rendered')))
+	     print('Post date:',postdt)
+	     print('Post content:', swaphtml(wppost.get('content').get('rendered')))
